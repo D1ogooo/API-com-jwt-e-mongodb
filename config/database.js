5d9618d7f96fd91b21a8e928ec436444096b8205
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2024-05-30 03:11:31 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-dotenv.config();
-
-main().catch(error => console.log(error));
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://diiogomarsalcosta:${process.env.PASSWORD_USER}@cluster0.blhhruo.mongodb.net/?retryWrites=true&w=majority`)
   try {
+    const password = process.env.MONGODB_PASSWORD; // qualquer variavel de ambiente que você for botar
+    const connectionString = `mongodb+srv://diiogomarsalcosta:${process.env.PASSWORD_USER}@cluster0.nayh0ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const client = new MongoClient(connectionString);
+    await client.connect()
     console.log("Conectado ao banco de dados")
   } catch (error) {
-    console.log("Falha ao se conectar com o banco de dados")
+    console.log(error.stack);
   }
 }
 
 
-module.exports = mongoose
+
+module.exports = main;
